c214705bfd9bfa348797b3db2053ae5aa7b4d61f
overstimulation <146885536+overstimulation@users.noreply.github.com>
2026-06-19 16:13:49 +0200

    next-greater-element-i

---
+++ arrays-and-hashing/next-greater-element-i.py
@@ -0,0 +1,11 @@
+class Solution:
+    def nextGreaterElement(self, nums1: list[int], nums2: list[int]) -> list[int]:
+        next_greater = {}
+        stack = []
+
+        for num in nums2:
+            while stack and stack[-1] < num:
+                next_greater[stack.pop()] = num
+            stack.append(num)
+
+        return [next_greater.get(num, -1) for num in nums1]
