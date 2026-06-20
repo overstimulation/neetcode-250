2608e9ac26da59df99b8fe25bce5d946962a8c5e
overstimulation <146885536+overstimulation@users.noreply.github.com>
2026-06-21 00:10:52 +0200

    maximum-ascending-subarray-sum

---
+++ arrays-and-hashing/maximum-ascending-subarray-sum.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maxAscendingSum(self, nums: list[int]) -> int:
+        max_sum = 0
+        current_sum = nums[0]
+        for i in range(1, len(nums)):
+            if nums[i] > nums[i - 1]:
+                current_sum += nums[i]
+            else:
+                max_sum = max(max_sum, current_sum)
+                current_sum = nums[i]
+        return max(max_sum, current_sum)
